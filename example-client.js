1489c77b97cd0fd82d0ff9582f5f81ea5d3fa673
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-03 16:50:11 -0500

    cleanup and updates

--- example-client.js
+++ example-client.js
@@ -1,16 +1,33 @@
-var domready = require('domready')
-var shoe = require('shoe')
-var dnode = require('dnode')
+var domready = require('domready');
+var shoe = require('shoe');
+var dnode = require('dnode');
+
+console.log("running script");
 
 domready(function () {
-	var stream = shoe('/dnode')
+	var stream = shoe('/dnode');
 	
-	var d = dnode()
+	var d = dnode();
 	d.on('remote', function (remote) {
-		console.log("successful connection")
-		remote.isAuthenticated(function (e, a) {
-			if (!e) console.log("api:",a)
-		})
-	})
-	d.pipe(stream).pipe(d)
+		console.log("successful connection");
+		if (!remote) console.log("REMOTE IS UNDEFINED THIS IS BAD!!!",remote);
+		else console.log('this is remote:',remote);
+		remote.createNewSession(function (e, a) {
+			console.log("create new session initiated")
+			if (!e) {
+				console.log("api:", a);
+				a.isAuthenticated(function(e, a) {
+					if (!e) {
+						console.log("Who is logged in:", a);
+					} else {
+						console.log("err:", e);
+					}
+				})
+			} else {
+				console.log("err:", e);
+			}
+		});
+	});
+	d.pipe(stream).pipe(d);
+	//d.end(); //NOOOOOOOOOO!!!!!!
 })
--- example-server.js
+++ example-server.js
@@ -1,7 +1,9 @@
 var dnode = require('dnode')
 var shoe = require('shoe')
+var http = require('http')
+var jlsa = require('just-login-server-api')
 
-var server = require('just-login-server-api')
+var server = http.createServer(jlsa.requestListener)
 server.listen(9999) //use whatever port you want
 
 var sock = shoe(function (stream) {
@@ -21,4 +23,12 @@ var sock = shoe(function (stream) {
 })
 sock.install(server, '/dnode') //name of socket?
 
+
+var sock = shoe(function (stream) {
+	var d = dnode(jlsa.api)
+	d.pipe(stream).pipe(d)
+})
+sock.install(server, '/dnode') //name of socket?
+
+module.exports = server
 //Checked
