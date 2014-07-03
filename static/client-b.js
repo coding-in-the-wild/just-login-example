0f51c11b2e163a15b7b385dbe229a74148cb6b32
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-03 16:49:31 -0500

    Changed build cmd to build client-b.js in /static

--- static/client-b.js
+++ static/client-b.js
@@ -3,19 +3,35 @@ var domready = require('domready');
 var shoe = require('shoe');
 var dnode = require('dnode');
 
+console.log("running script");
+
 domready(function () {
-	var result = document.getElementById('result');
 	var stream = shoe('/dnode');
 	
 	var d = dnode();
 	d.on('remote', function (remote) {
-		console.log("successful connection")
-		remote.isAuthenticated(function (e, a) {
-			if (!e) console.log("api:",a)
+		console.log("successful connection");
+		if (!remote) console.log("REMOTE IS UNDEFINED THIS IS BAD!!!",remote);
+		else console.log('this is remote:',remote);
+		remote.createNewSession(function (e, a) {
+			console.log("create new session initiated")
+			if (!e) {
+				console.log("api:", a);
+				a.isAuthenticated(function(e, a) {
+					if (!e) {
+						console.log("Who is logged in:", a);
+					} else {
+						console.log("err:", e);
+					}
+				})
+			} else {
+				console.log("err:", e);
+			}
 		});
 	});
 	d.pipe(stream).pipe(d);
-});
+	//d.end(); //NOOOOOOOOOO!!!!!!
+})
 
 },{"dnode":2,"domready":13,"shoe":14}],2:[function(require,module,exports){
 var dnode = require('./lib/dnode');
