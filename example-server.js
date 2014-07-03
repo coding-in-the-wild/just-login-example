77cfb54864824a9e396655066fc47ddb4826f804
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-03 16:01:41 -0500

    use the server-api that returns the server

--- example-server.js
+++ example-server.js
@@ -1,24 +1,8 @@
 var dnode = require('dnode')
-var http = require('http')
 var shoe = require('shoe')
-var url = require('url')
-var send = require('send')
 
-var server = http.createServer().on('request', function(req, res) {
-	send(req, url.parse(req.url).pathname, {root: './static'})
-		.on('error', function(err) {
-			console.log("bad url or something:",err.message)
-		}).on('file', function(path, stat) {
-			console.log("file req:",path)
-		}).on('directory', function() {
-			res.statusCode = 301;
-			res.setHeader('Location', req.url + '/');
-			res.end('Redirecting to ' + req.url + '/');
-		})
-		//.on('headers', headers)
-		.pipe(res)
-})
-server.listen(9999)
+var server = require('just-login-server-api')
+server.listen(9999) //use whatever port you want
 
 var sock = shoe(function (stream) {
 	var loggedIn = false
