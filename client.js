4306b75ff2cec13f70bd075a638f16548bf180b4
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-08 22:10:23 -0500

    update stuffz

--- client.js
+++ client.js
@@ -1,29 +1,23 @@
-var createSession = require('just-login-client')
-var Jlsa = require('just-login-server-api')
-var Jlc = require('just-login-core')
-var Level = require('level-mem')
-var db = Level('uniqueNameHere')
-var jlc = Jlc(db)
-var jlsa = Jlsa(jlc)
 var sendEmailOnAuth = require('./emailWrapper.js')
+var dnode = require('dnode')
 
+var d = dnode()
+d.on('remote', function (remote) {
+	console.log("successful connection")
 
-//client(function (remote) { //connect here!
-	//console.log("successful connection")
+	remote.createSession(function (err, api, sessionId) {
+		console.log("create new session initiated")
+		if (err) {
+			console.log("err:", err)
+		} else {
+			console.log("api:", api)
+			var emitter = api.beginAuthentication("fake@example.com")
+			sendEmailOnAuth(emitter)
+			emitter.on('authentication initiated', function (authInit) {
+				console.log(authInit.token)     //logs the secret token (um bad idea?)
+				console.log(authInit.sessionId) //logs the session id
+			})
+		}
+	})
 
-createSession(jlsa, function (err, api, sessionId) {
-	console.log("create new session initiated")
-	if (err) {
-		console.log("err:", err)
-	} else {
-		console.log("api:", api)
-		var emitter = api.beginAuthentication("fake@example.com")
-		sendEmailOnAuth(emitter)
-		emitter.on('authentication initiated', function (authInit) {
-			console.log(authInit.token)     //logs the secret token
-			console.log(authInit.sessionId) //logs the session id
-		})
-	}
 })
-
-//})
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var url = require('url')
+var http = require('http')
 var send = require('send')
-var api = require('just-login-server-api')
+var createSession = require('just-login-client')
+var Jlsa = require('just-login-server-api')
+var Jlc = require('just-login-core')
+var Level = require('level-mem')
+var db = Level('uniqueNameHere')
+var jlc = Jlc(db)
+var jlsa = Jlsa(jlc)
 var dnode = require('dnode')
+var shoe = require('shoe')
 
 var requestListener = function requestListener(req, res) {
 	send(req, url.parse(req.url).pathname, {root: "./static/"})
@@ -14,11 +22,11 @@ var requestListener = function requestListener(req, res) {
 		.on('directory', function() {
 			console.log("directory")
 			res.statusCode = 301;
-			res.setHeader('Location', req.url + '/');
-			res.end('Redirecting to ' + req.url + '/');
+			res.setHeader('Location', req.url + '/')
+			res.end('Redirecting to ' + req.url + '/')
 		}).on('headers', function (res, path, stat) {
 			console.log('headers')
-			//res.setHeader('Content-Disposition', 'attachment'); //this made me download the file
+			//res.setHeader('Content-Disposition', 'attachment'); //this made me download the file lol
 		})
 		.pipe(res)
 }
@@ -26,49 +34,10 @@ var requestListener = function requestListener(req, res) {
 var server = http.createServer(requestListener)
 server.listen(9999)
 
-	/*
-	var sock = shoe(function (stream) {
-		var d = dnode(api)
-		d.pipe(stream).pipe(d)
-	})
-	sock.install(server, '/dnode')*/
-	
-	/*var dnode = require('dnode');
-	var net = require('net');
-
-	var server = net.createServer(function (c) {
-		var d = dnode(api);
-		c.pipe(d).pipe(c);
-	});
-
-	server.listen(5004);*/
-module.exports = server
-
-/*
-var dnode = require('dnode')
-var browserApi = require('./browserApi.js')
-var fs = require('fs')
-var http = require('http')
-var shoe = require('shoe')
-//var send = require('send')
-
-
-//var server = http.createServer();
-//server.listen(9999);
-//server.on('request', function(req, res) {
-var server = http.createServer().listen(9999).on('request', function(req, res) {
-	console.log("connection initiated")
-	fs.readFile("./client-b.js", {encoding:'utf8'}, function(err, data) {
-		if (err)
-			res.end('<h1>joseph</h1>', 'utf8')
-		else
-			res.end("<script>"+data+"</script>", 'utf8')
-	})
-})
-
 var sock = shoe(function (stream) {
-	var d = dnode(browserApi)
+	var d = dnode(jlsa)
 	d.pipe(stream).pipe(d)
 })
 sock.install(server, '/dnode')
-*/
+
+module.exports = server
