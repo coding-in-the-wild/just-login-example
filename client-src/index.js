4fafccc8130db44fe53021a10e276631db2c1541
Josh Duff <me@JoshDuff.com>
2014-07-18 02:21:34 +0000

    A dumb first layout of the ui

--- client-src/index.js
+++ client-src/index.js
@@ -1,10 +1,32 @@
 var client = require('just-login-client')
+var classes = require('dom-classes')
+var on = require('dom-event')
 
-
-client(function (err, api, sessionId) {
+var emitter = client(function (err, api, sessionId) {
 	if (err) {
 		console.log(err.message)
 	} else {
+		api.isAuthenticated(function(err, name) {
+			console.log("got back", err, name)
+			if (name) {
+				alert("oh hey you were logged in already as " + name)
+			} else {
+				var btn = document.getElementById('login-button')
+				classes.remove(btn, 'pure-button-disabled')
 
+				on(btn, 'click', function() {
+					var emailAddress = document.getElementById('login-email-address').value
+					api.beginAuthentication(emailAddress, function(err, obj) {
+						console.log(err, obj)
+					})
+				})
+			}
+		})
 	}
 })
+
+emitter.on('authenticated', function(name) {
+	alert("You're logged in as " + name)
+})
+
+//emitter.on('continue session')
