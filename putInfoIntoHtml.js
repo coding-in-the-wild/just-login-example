89be460785f40948ec26275183b0ace09063a82f
Joseph Dykstra <josephdykstra@gmail.com>
2014-11-06 20:31:55 -0600

    Created documentation template ignoring static/index.html from now on (it gets built in a build step) deleted embedded html from info.md added brucedown as a dev dependency added documentation insertion program (putInfoIntoHtml.js) added documentation insertion to npm prestart script

---
+++ putInfoIntoHtml.js
@@ -0,0 +1,22 @@
+var fs = require('fs')
+var brucedown = require('brucedown')
+
+
+fs.readFile('./info.md', {encoding: 'utf8'}, function (err, infoMd) {
+	if (err) throw err
+
+	brucedown(infoMd, function (err, html) {
+		if (err) throw err
+
+		fs.readFile('./indexTemplate.html', {encoding: 'utf8'}, function (err, indexHtml) {
+			if (err) throw err
+
+			var fullHtml = indexHtml.replace('<!--DOCUMENTATION_GOES_HERE-->', html)
+
+			fs.writeFile('./static/index.html', fullHtml, function (err) {
+				if (err) throw err
+			})	
+		})
+	})
+})
+
