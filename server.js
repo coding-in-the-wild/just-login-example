5841803e7593d2bf6c9259c1ca786ad626f05635
ArtskydJ <josephdykstra@gmail.com>
2015-04-04 19:48:13 -0400

    use passed in port

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var config = require('confuse')().justLogin
 Level(__dirname + '/mydb', function (err, db) {
 	if (err) throw err
 	var server = require(__dirname + '/server-src/index.js')
-	server(db).listen(process.env.PORT || config.port || 80)
+	server(db).listen(process.argv[2] || config.port || 80)
 })
