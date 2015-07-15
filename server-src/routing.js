8b4c698fa5902c071bf5628cb65be8d59b130fed
Joseph Dykstra <josephdykstra@gmail.com>
2015-07-15 09:03:43 -0500

    cleaned up the routing

--- server-src/routing.js
+++ server-src/routing.js
@@ -1,40 +1,28 @@
-var send = require('send')
-var Router = require('router')
 var url = require('url')
+var ecstatic = require('ecstatic')
+require('string.prototype.startswith')
 
 var config = require('confuse')().justLogin
-var SEND_OPTS = { root: config.staticDir }
-
-function serve(req, res, file) {
-	var parsed = url.parse(req.url)
-	if (typeof file !== 'string') file = parsed.pathname
-	res.statusCode = 200 // Status code
-	send(req, file, SEND_OPTS).pipe(res).on('error', handle)
-
-	function handle(err) {
-		res.statusCode = err.status || 500
-		send(req, '/404.html', SEND_OPTS).pipe(res)
-	}
-}
+var serve = ecstatic({
+	root: config.staticDir
+})
 
 module.exports = function (core) {
-	var router = Router()
-
-	router.get(config.endpoints.dnode, function () {})
-	router.get(config.endpoints.custom, function () {})
-	router.get(config.endpoints.token, function (req, res) {
-		var parsed = url.parse(req.url, true)
-		var token = parsed.query.token
-		core.authenticate(token, function (err, addr) {
-			if (err) { //Bad token, and other errors
-				serve(req, res, 'loginFailure.html')
-			} else {
-				serve(req, res, 'loginSuccess.html')
-			}
-		})
-	})
-
 	return function requestHandler(req, res) {
-		return router(req, res, function () { serve(req, res) })
+		var parsed = url.parse(req.url, true)
+		var authAttempt = parsed.pathname.startsWith(config.endpoints.token)
+
+		if (authAttempt) {
+			var token = parsed.query.token
+			core.authenticate(token, function (err, addr) {
+				var pathname = err ? '/loginFailure.html' : '/loginSuccess.html'
+
+				res.statusCode = 307
+				res.setHeader('Location', url.resolve(req.url, pathname))
+				res.end()
+			})
+		} else {
+			serve(req, res)
+		}
 	}
 }
