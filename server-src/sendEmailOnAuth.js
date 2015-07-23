9c14da699ea81b6a7e1a31eda1b3adb57bfc9c45
Joseph Dykstra <josephdykstra@gmail.com>
2015-07-23 08:28:55 -0500

    more readable

--- server-src/sendEmailOnAuth.js
+++ server-src/sendEmailOnAuth.js
@@ -1,13 +1,13 @@
 var JustLoginEmailer = require('just-login-emailer')
-var compile = require('string-template/compile')
+var stCompile = require('string-template/compile')
 var formatUrl = require('url').format
 var fs = require('fs')
 var path = require('path')
 var config = require('../config.json')
 var transportConfig = process.env.CI ? {} : require('./transport-config.json')
 
-var emailTemplate = fs.readFileSync(path.resolve(__dirname, 'emailTemplate.html'), 'utf8')
-emailTemplate = compile(emailTemplate)
+var emailTemplateString = fs.readFileSync(path.resolve(__dirname, 'emailTemplate.html'), 'utf8')
+var emailTemplate = stCompile(emailTemplateString)
 
 module.exports = function(core, baseUrl) {
 	function htmlEmail(loginToken) {
